90340dc64a2f6782af4edbb87ff46cc8b583750e
Shannon Young <sayoung0820@gmail.com>
2025-06-03 21:09:00 -0400

    Completed Shopping Cart IO exercise

---
+++ Console IO Exercises/shopping_cart_consol_io/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Are you tax exempt? (y/n) ");
+        String exempt = console.nextLine();
+
+        System.out.println("Will you require shipping? (y/n) ");
+        String shipping = console.nextLine();
+
+        System.out.println("Please enter your order quantity: ");
+        String quantityStr = console.nextLine();
+        int quantity = Integer.parseInt(quantityStr);
+
+        System.out.println("Please enter the promo code for free shipping or type NONE: ");
+        String code = console.nextLine();
+
+        System.out.println("                                         ");
+
+        System.out.println("Details: ");
+
+        System.out.println("Tax Exempt: " + exempt);
+        System.out.println("Shipping required: " + shipping);
+        System.out.println("Quantity of products: " + quantity);
+        System.out.println("Promo Code: " + code);
+
+        System.out.println("Bye!");
+
+
+
+
+
+
+    }
+}
