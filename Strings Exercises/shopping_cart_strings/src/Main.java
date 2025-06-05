fbd539c20ca12820b68f64f390ff00ff17140b26
Shannon Young <sayoung0820@gmail.com>
2025-06-05 18:10:12 -0400

    Shopping Cart Strings Exercise Completed

---
+++ Strings Exercises/shopping_cart_strings/src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+
+    enum OrderStatus {
+        PENDING,
+        PROCESSING,
+        SHIPPED,
+        DELIVERED
+    }
+
+    enum ShippingStatus {
+        STANDARD,
+        TWO_DAY,
+        OVERNIGHT
+    }
+
+    public static void main(String[] args) {
+
+        String businessName = "My Business";
+        String businessContactInfo = "123-456-7890";
+        String productDescription = "My product description";
+
+        System.out.print("Business name: " + businessName);
+        System.out.println();
+
+        System.out.println("Business contact info: " + businessContactInfo);
+        System.out.println();
+
+        System.out.println("Product description: " + productDescription);
+        System.out.println();
+
+
+        System.out.println(ShippingStatus.STANDARD);
+        System.out.println(ShippingStatus.TWO_DAY);
+        System.out.println(ShippingStatus.OVERNIGHT);
+        OrderStatus orderStatus = OrderStatus.PROCESSING;
+        System.out.println("Oder Status" + orderStatus);
+
+        System.out.println(OrderStatus.PENDING);
+        System.out.println(OrderStatus.PROCESSING);
+        System.out.println(OrderStatus.SHIPPED);
+        System.out.println(OrderStatus.DELIVERED);
+        ShippingStatus shippingStatus = ShippingStatus.TWO_DAY;
+        System.out.println("Ship Status: " + shippingStatus);
+
+
+        // Assign variables
+        int productID = 1;
+        int productCategory = 2;
+        double productCost = 2.56;
+        double productPrice = 4.99;
+        int productQuantity = 78;
+
+        // Write code to calculate total cost of the product based on the inventory
+        // Print the value
+        double totalCost = productCost * productQuantity;
+        System.out.println("The total cost of the product is: " + totalCost);
+
+        // Calculate profit of the product. Print value.
+        double profitMargin = productPrice - productCost;
+        System.out.println("The Profit in dollars is: " + profitMargin);
+
+        // Calculate the total potential profit. Print value
+        double totalProfit = profitMargin * productQuantity;
+        System.out.println("The total Potential Profit is: " + totalProfit);
+
+    }
+}
